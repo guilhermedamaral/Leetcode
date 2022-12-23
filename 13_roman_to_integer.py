0fa3b5ffa42cf7c973d56eb4a876f172912b4090
guilhermedamaral <guilherme-direito@hotmail.com>
2022-12-23 11:34:17 -0300

    13

--- 13_roman_to_integer.py
+++ 13_roman_to_integer.py
@@ -1,22 +1,19 @@
 
-def romanToInt(s):
-    letters = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M': 1000}
-    value, ancestor = 0, 1001
-    for let in s:
-        if letters[let] > ancestor and ancestor > 0:
-            value += letters[let]
-            value -= ancestor*2
-        
-        else:
-            value += letters[let]
-            ancestor = letters[let]
+def romanToInt(values):
+    for s in values:
+        letters = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M': 1000}
+        value, ancestor = 0, 1001
+        for let in s:
+            if letters[let] > ancestor and ancestor > 0:
+                value += letters[let]
+                value -= ancestor*2
+            
+            else:
+                value += letters[let]
+                ancestor = letters[let]
 
-    print(value)
-    return value
+        print(f"{s}: {value}")
+    return
 
-s = "III"
-romanToInt(s)
-s = "LVIII"
-romanToInt(s)
-s = "MCMXCIV"
-romanToInt(s)
+values = ["III", "LVIII", "MCMXCIV"]
+romanToInt(values)
